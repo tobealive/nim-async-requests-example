12c6f01718b339555d0a0ee8b785e7e807b05de1
Turiiya <34311583+tobealive@users.noreply.github.com>
2023-03-15 01:46:10 +0100

    format

--- src/async_requests.nim
+++ src/async_requests.nim
@@ -1,12 +1,13 @@
 #? replace(sub = "\t", by = "  ")
+# nim r -d:ssl -d:release src/async_requests.nim
 import asyncdispatch, httpclient, strformat, times, strutils, sequtils, sugar
 
-let
-	url_source = "https://gist.githubusercontent.com/tobealive/b2c6e348dac6b3f0ffa150639ad94211/raw/3db61fe72e1ce6854faa025298bf4cdfd2b2f250/100-popular-urls.txt"
+const
+	urlSource = "https://gist.githubusercontent.com/tobealive/b2c6e348dac6b3f0ffa150639ad94211/raw/3db61fe72e1ce6854faa025298bf4cdfd2b2f250/100-popular-urls.txt"
 	seperator = "-".repeat(80)
 	iterations = 10
-	single_source = false
-	response_timeout = 10000
+	singleSource = false
+	responseTimeout = 10000
 	verbose = true
 
 type
@@ -19,30 +20,30 @@ var
 	outputs: seq[string]
 
 
-proc prep_urls(): seq[string] =
-	if single_source:
+proc prepUrls(): seq[string] =
+	if singleSource:
 		return collect newSeq: (for i in 1..100: (&"google.com/search?q={i}"))
 	try:
-		result = newHttpClient().getContent(url_source).splitLines.deduplicate()
+		result = newHttpClient().getContent(urlSource).splitLines.deduplicate()
 		if result.len > 100:
 			result.setLen(100)
 	except Exception as e:
 		echo &"Error: {e.name}"
 
 
-proc get_http_resp(url: string): Future[TestResult] {.async.} =
+proc getHttpResp(url: string): Future[TestResult] {.async.} =
 	result.url = url
 	let client = newAsyncHttpClient()
-	let start_time = epochTime()
+	let startTime = epochTime()
 
 	try:
 		let request = client.get(&"http://www.{url}")
-		if await request.withTimeout(response_timeout):
+		if await request.withTimeout(responseTimeout):
 			let resp = await request
 			let body = await resp.body
 			result.status = success
 			result.transferred = body.len
-			result.time = epochTime() - start_time
+			result.time = epochTime() - startTime
 			if verbose:
 				echo &"{result.url} — Transferred: {result.transferred} Bytes. Time: {result.time:.2f}s."
 		else:
@@ -51,15 +52,15 @@ proc get_http_resp(url: string): Future[TestResult] {.async.} =
 				echo &"TIMEOUT: {url}"
 	except Exception as e:
 		result.status = error
-		result.time = epochTime() - start_time
+		result.time = epochTime() - startTime
 		if verbose:
 			echo &"ERROR: {result.url} — {e.name}. Time: {result.time:.2f}s."
 	finally:
 		client.close()
 
 
-proc spawn_requests(urls: seq[string]): Future[seq[TestResult]] {.async.} =
-	let futures: seq[Future[TestResult]] = collect newSeq: (for url in urls: get_http_resp(url))
+proc spawnRequests(urls: seq[string]): Future[seq[TestResult]] {.async.} =
+	let futures: seq[Future[TestResult]] = collect newSeq: (for url in urls: getHttpResp(url))
 	result = waitFor all(futures)
 
 
@@ -84,18 +85,18 @@ proc eval(results: seq[TestResult]): Stats =
 
 
 proc main() =
-	let urls = prep_urls()
+	let urls = prepUrls()
 	echo "Starting requests..."
 
 	for i in 1..iterations:
 		echo &"Run: {i}/{iterations}"
 
-		let start_time = epochTime()
-		let results = waitFor spawn_requests(urls)
-		let end_time = epochTime()
+		let startTime = epochTime()
+		let results = waitFor spawnRequests(urls)
+		let endTime = epochTime()
 
 		var stats = eval(results)
-		stats.time = end_time - start_time
+		stats.time = endTime - startTime
 		summary.time += stats.time
 
 		let output = (&"{i}: Time: {stats.time:.2f}s. " &
